5e6b4219f6d4afb5b41f3e13d435832c67d61fc6
MaDMikeNsk <mikemenshikov85@gmail.com>
2019-01-25 22:50:16 +0300

    2

---
+++ src/Cat.java
@@ -0,0 +1,120 @@
+
+public class Cat
+{
+    private Double originWeight;
+    private Double weight;
+    private static int count = 0;
+
+    private Double minWeight;
+    private Double maxWeight;
+
+    private Boolean isDead;
+    private Double fixedWeight;
+
+
+
+    public Cat()
+    {
+        weight = 1500.0 + 3000.0 * Math.random();
+        originWeight = weight;
+        minWeight = 1000.0;
+        maxWeight = 9000.0;
+        count ++;
+        isDead = false;
+    }
+
+    public Cat ( Double fixedWeight)     //Кошка с заданной массой
+    {
+        weight = fixedWeight;
+        originWeight = fixedWeight;
+        isDead = false;
+        count ++;
+        minWeight = 50.0;
+        maxWeight = 200.0;
+    }
+
+    public void meow()
+    {
+        weight = weight - 1;
+//        System.out.println("Meow");
+    }
+
+    public void feed(Double amount)
+    {
+        weight = weight + amount;
+    }
+
+    public void drink(Double amount)
+    {
+        weight = weight + amount;
+    }
+
+    public Double getWeight()
+    {
+        return weight;
+    }
+
+    public String getStatus()
+    {
+        String status="";
+        if (isDead) {                             //Кошка уже умерла
+            if (weight > maxWeight) {
+                status = "Exploded";
+            } else if (weight < minWeight) {
+                status = "Dead";
+            }
+        } else {                                  //Кошка не умерла
+            if (weight < minWeight) {
+                count--;
+                isDead = true;
+                status = "Dead";
+            } else if (weight > maxWeight) {
+                count--;
+                isDead = true;
+                status = "Exploded";
+            } else if (weight > originWeight) {
+                status = "Sleeping";
+            } else {
+                status = "Playing";
+            }
+        }
+        return status;
+    }
+
+    public Double getFeedAmount()
+    {
+        return weight - originWeight;
+    }
+
+    public void goToilet ()
+    {
+        weight = weight - 10;
+        System.out.println("Кошка оправилась, вес уменьшился на 10 грамм :)");
+    }
+
+    public static int getCount ()
+    {
+        return count;
+    }
+
+    public static Double getWeightDifference (Cat cat1, Cat cat2)
+    {
+        Double difference = Math.abs (cat1.getWeight() - cat2.getWeight());
+        return difference;
+    }
+
+    public static Cat getNewRandomCat()       //Метод создания новой кошки со случайной массой
+    {
+        Double randomWeight = 100 + 100*Math.random();
+        Cat cat = new Cat(randomWeight);
+        return cat;
+    }
+
+    public static Cat cloneCat (Cat cat)      //Метод создания 'глубокой' копии кошки
+    {
+        Cat clone = new Cat();
+        clone.weight = cat.weight;
+        clone.originWeight = cat.originWeight;
+        return clone;
+    }
+}
---
+++ src/Loader.java
@@ -0,0 +1,114 @@
+import java.net.Socket;
+
+public class Loader
+{
+    public static void main(String[] args)
+    {
+        Cat murzik = new Cat();
+        Cat vaska = new Cat();
+        Cat tishka = new Cat();
+        Cat barsik = new Cat();
+        Cat bob = new Cat();
+        Cat dik = new Cat();
+        Cat masya = new Cat();
+        Cat black = new Cat(110.0);   //Создаём кошку с заданным весом
+
+        System.out.println("Кошек создано: " + Cat.getCount());
+
+        System.out.println("=============================================");
+        //**********************************************************************************
+        //Кормим Мурзика
+        System.out.println("Вес кота Мурзика до: " + murzik.getWeight());
+        for (int i=1; i<=500; i++){
+            murzik.feed(8.0);
+        }
+        System.out.println("Вес кота Мурзика после: " + murzik.getWeight());
+        System.out.println("Съедено: " + murzik.getFeedAmount() + " г");
+        System.out.println("Мурзик " + murzik.getStatus());
+        System.out.println("Количество кошек: " + Cat.getCount());
+        System.out.println("=============================================");
+
+        //***********************************************************************************
+        //Васька 500 раз мяукает
+        System.out.println("Вес кота Васьки до: " + vaska.getWeight());
+        for (int i=1; i<=500; i++) {
+            vaska.meow();
+        }
+        System.out.println("Вес кота Васьки после: " + vaska.getWeight());
+        System.out.println("Васька " + vaska.getStatus());
+        System.out.println("Количество кошек: " + Cat.getCount());
+        System.out.println("=============================================");
+
+        //************************************************************************************
+        //Взрываем Тишку
+        System.out.println("Вес кота Тишки до: " + tishka.getWeight());
+        int i=0;
+        while (!tishka.getStatus().equals("Exploded")) {
+            tishka.feed(8.0);
+            i++;
+        }
+        System.out.println("Количество приёмов пищи: " + i);
+        System.out.println("Съедено " + tishka.getFeedAmount() + " грамм");
+        System.out.println("Тишка " + tishka.getStatus());
+        System.out.println("Количество кошек: " + Cat.getCount());
+        System.out.println("=============================================");
+
+        //*************************************************************************************
+        //Барсик мяукает до смерти
+        System.out.println("Вес кота Барсика до: " + barsik.getWeight());
+        int j=0;
+        while (!barsik.getStatus().equals("Dead")) {
+            barsik.meow();
+            j++;
+        }
+        System.out.println("Количество мяуканий: " + j);
+        System.out.println("Барсик " + barsik.getStatus());
+        System.out.println("Количество кошек: " + Cat.getCount());
+        System.out.println("=============================================");
+        //*************************************************************************************
+
+        bob.goToilet();
+        System.out.println("=============================================");
+
+        System.out.println("Количество кошек: " + Cat.getCount());
+//
+//        System.out.println("Вес Боба: " + bob.getWeight());
+//        System.out.println("Вес Дика: "+ dik.getWeight());
+//        System.out.println(Cat.getWeightDifference(bob, dik));
+
+        System.out.println("Мурзик " + murzik.getStatus());
+        System.out.println("Васька " + vaska.getStatus());
+        System.out.println("Тишка " + tishka.getStatus());
+        System.out.println("Барсик " + barsik.getStatus());
+        System.out.println("Боб " + bob.getStatus());
+        System.out.println("Дик " + dik.getStatus());
+        System.out.println("Мася " + masya.getStatus());
+        System.out.println("Black " + black.getStatus());
+        System.out.println("=============================================");
+        //***************************************************************************************
+
+        //Эксперименты с кошкой Black заданной массы
+        System.out.println(black.getWeight());
+        black.feed(150.0);
+        System.out.println(black.getWeight());
+        System.out.println(black.getStatus());
+        System.out.println("Количество кошек: " + Cat.getCount());
+        System.out.println("=============================================");
+
+
+        //***********   Создаём новую кошку со случайной массой в диапазоне [100,200] грамм    *********************
+        Cat white = Cat.getNewRandomCat();
+        System.out.println(white.getWeight());
+        System.out.println("Количество кошек: " + Cat.getCount());
+        System.out.println("=============================================");
+
+        //*****************      Клонируем кошку    **************************
+        Cat blue = new Cat();
+        Cat dark = Cat.cloneCat (blue);
+        blue.feed(100.0);
+        System.out.println(blue.getWeight());
+        System.out.println(dark.getWeight());
+        //**************************************************************************
+
+    }
+}
